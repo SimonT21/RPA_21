170482f2d956f0fd86f4390cc9a198f6e884c67c
Simon T <bulofon0108@gmail.com>
2021-01-20 17:28:56 +0100

    Prvi projekt

---
+++ Test za Git/Test za Git/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_za_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdravljen Git");
+            Console.ReadLine();
+        }
+    }
+}
